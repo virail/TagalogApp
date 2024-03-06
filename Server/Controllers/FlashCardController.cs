b0b7b354e209be5b83a167ed13061df9ad001922
James Bridge <sc19jb@leeds.ac.uk>
2024-03-06 23:19:05 +0000

    added basic flash card template

---
+++ Server/Controllers/FlashCardController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TagalogApp.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FlashCardController : ControllerBase
+{
+
+    
+
+}
---
+++ Shared/FlashCard.cs
@@ -0,0 +1,10 @@
+namespace TagalogApp.Shared;
+
+public class FlashCard
+{
+
+    public long Id { get; set; }
+    public string? Front { get; set; }
+    public string? Back { get; set; }
+
+}
